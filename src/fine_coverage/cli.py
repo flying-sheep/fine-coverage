6403cd86684a4b2682828e29edb44ac05a3547d8
Philipp A <flying-sheep@web.de>
2022-12-11 14:59:46 +0100

    WIP CLI

--- src/fine_coverage/cli.py
+++ src/fine_coverage/cli.py
@@ -1,6 +1,21 @@
+import sys
+from pathlib import Path
+from runpy import run_module
+
+import rich
+
+from .report import TraceHighlighter
 from .tracer import Tracer
+
+HERE = Path(__file__).parent
 
 
 def run():
-    with Tracer():
-        pass
+    argv = sys.argv
+    mod = argv[1]
+    sys.argv = [sys.executable, '-m', mod, *argv[2:]]
+    with Tracer() as tracer:
+        run_module(mod)
+    highlighter = TraceHighlighter(tracer.events)
+    rich.print(highlighter.highlight_mod(str(HERE / 'ast.py')))
+    sys.argv = argv
---
+++ src/fine_coverage/report.py
@@ -0,0 +1,33 @@
+from collections.abc import Collection
+from dataclasses import dataclass, field
+from pathlib import Path
+
+from rich.highlighter import Highlighter
+from rich.text import Text
+
+from .ast import Span, parse
+from .tracer import CodeLocs
+
+
+@dataclass
+class TraceHighlighter(Highlighter):
+    events: Collection[CodeLocs]
+    spans: dict[str, list[Span]] = field(init=False)
+
+    def __post_init__(self):
+        file_paths = {locs.file for locs in self.events}
+        self.spans = {file_path: list(parse(file_name=file_path)) for file_path in file_paths}
+
+    def highlight_mod(self, file_path: str) -> Text:
+        # TODO: UTF-8
+        text = Text(Path(file_path).read_text().splitlines())
+        for span in self.spans[file_path]:
+            start, end = span.start.col, span.end.col  # TODO: translate line positions to indices
+            covered = any(
+                loc == span
+                for event in self.events
+                if event.file == file_path
+                for loc in event.locs
+            )
+            text.stylize('green' if covered else 'red', start, end)
+        return text
