4393a84fb6978e572d992a1287c9060328fdb151
Philipp A <flying-sheep@web.de>
2022-12-12 12:07:49 +0100

    almost

--- src/fine_coverage/report.py
+++ src/fine_coverage/report.py
@@ -1,11 +1,34 @@
-from collections.abc import Collection
+from __future__ import annotations
+
+from collections.abc import Callable, Collection, Generator, Iterable
 from dataclasses import dataclass, field
 from pathlib import Path
+from typing import TypeVar
 
 from rich.text import Text
 
-from .ast import Span, parse
+from .ast import Pos, Span, parse
 from .tracer import CodeLocs
+
+T = TypeVar('T')
+
+
+def intersperse(
+    items: Iterable[T], fn: Callable[[T | None, T | None], T]
+) -> Generator[T, None, None]:
+    it = iter(items)
+    prev = None
+    next_ = None
+    while True:
+        try:
+            next_ = next(it)
+        except StopIteration:
+            yield fn(next_, None)
+            break
+        else:
+            yield fn(prev, next_)
+            yield next_
+        prev = next_
 
 
 @dataclass
@@ -18,15 +41,35 @@ class TraceHighlighter:
         self.spans = {file_path: list(parse(file_name=file_path)) for file_path in file_paths}
 
     def highlight_mod(self, file_path: str) -> Text:
-        # TODO: UTF-8
-        text = Text(Path(file_path).read_text())
-        for span in self.spans[file_path]:
-            start, end = span.start.col, span.end.col  # TODO: translate line positions to indices
-            covered = any(
-                loc == span
-                for event in self.events
-                if event.file == file_path
-                for loc in event.locs
+        lines = Path(file_path).read_bytes().splitlines()
+        text = Text()
+        for l, line in enumerate(lines, 1):
+            spans = sorted(
+                [
+                    span
+                    for span in self.spans[file_path]
+                    if span.start.line <= l and span.end.line >= l
+                ],
+                key=lambda span: span.start,
             )
-            text.stylize('green' if covered else 'red', start, end)
+            span_styles = intersperse(
+                [(span, 'green' if self.covered(file_path, span) else 'red') for span in spans],
+                lambda prev, next_: (
+                    Span(
+                        Pos(l, 0 if prev is None else prev[0].end.col),
+                        Pos(l, len(line) if next_ is None else next_[0].start.col),
+                    ),
+                    None,
+                ),
+            )
+            for span, style in span_styles:
+                start = 0 if span.start.line < l else span.start.col
+                end = len(line) if span.end.line > l else span.end.col
+                text.append(line[start:end].decode('utf-8'), style)
+            text.append('\n')
         return text
+
+    def covered(self, file_path: str, span: Span) -> bool:
+        return any(
+            loc == span for event in self.events if event.file == file_path for loc in event.locs
+        )
