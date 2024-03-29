ff3e85624ccb7e99d1283444f04d5ee6cd8469d1
Philipp A. <flying-sheep@web.de>
2024-03-29 19:51:41 +0100

    run

---
+++ src/collector.rs
@@ -0,0 +1,37 @@
+use pyo3::prelude::*;
+
+use pyo3::types::PyFrame;
+
+use crate::tracer::{TraceEvent, Tracer};
+
+#[pyclass]
+#[derive(Default)]
+pub struct Collector {}
+
+impl Tracer<TraceEvent> for Collector {
+    fn trace(&mut self, frame: Py<PyFrame>, event: TraceEvent, py: Python) -> PyResult<()> {
+        let frame = frame.into_bound(py);
+        match event {
+            TraceEvent::Exception {
+                exc_type,
+                exc_value,
+                exc_traceback,
+            } => {
+                dbg!(
+                    "exception",
+                    frame,
+                    exc_type.into_bound(py),
+                    exc_value.into_bound(py),
+                    exc_traceback.into_bound(py)
+                );
+            }
+            TraceEvent::Return(value) => {
+                dbg!("return", frame, value.map(|value| value.into_bound(py)));
+            }
+            _ => {
+                dbg!(event, frame);
+            }
+        }
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #![warn(clippy::pedantic)]
 
+mod collector;
 mod tracer;
 
 use pyo3::exceptions::PySystemExit;
 use pyo3::prelude::*;
 
 use clap::Parser;
-use pyo3::types::PyFrame;
 
 use crate::tracer::Register;
 
@@ -26,46 +26,24 @@ struct Args {
     cov: Option<String>,
 }
 
-#[pyclass]
-struct Tracer;
-
-impl tracer::Tracer<tracer::TraceEvent> for Tracer {
-    fn trace(&mut self, frame: Py<PyFrame>, event: tracer::TraceEvent, py: Python) -> PyResult<()> {
-        let frame = frame.into_bound(py);
-        match event {
-            tracer::TraceEvent::Exception {
-                exc_type,
-                exc_value,
-                exc_traceback,
-            } => {
-                dbg!(
-                    "exception",
-                    frame,
-                    exc_type.into_bound(py),
-                    exc_value.into_bound(py),
-                    exc_traceback.into_bound(py)
-                );
-            }
-            tracer::TraceEvent::Return(value) => {
-                dbg!("return", frame, value.map(|value| value.into_bound(py)));
-            }
-            _ => {
-                dbg!(event, frame);
-            }
-        }
-        Ok(())
-    }
-}
-
 /// Runs the command line interface.
 #[pyfunction]
 fn cli() -> PyResult<()> {
     // TODO: color
     let args = Args::try_parse().map_err(|e| PySystemExit::new_err(e.to_string()))?;
-    let tracer = Tracer {};
-    Python::with_gil(|py| Bound::new(py, tracer)?.register())?;
-    // TODO: run module
-    args.module;
+    let collector = collector::Collector::default();
+    Python::with_gil(|py| {
+        Bound::new(py, collector)?.register()?;
+
+        let runpy = PyModule::import_bound(py, "runpy")?;
+        let run_fn = if args.module {
+            "run_module"
+        } else {
+            "run_path"
+        };
+        runpy.getattr(run_fn)?.call((args.name,), None)?;
+        PyResult::Ok(())
+    })?;
     Ok(())
 }
 
